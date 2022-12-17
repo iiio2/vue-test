b08a1cb5070f2402a2a7419e72ea3c22758a98de
Mr. Robot <gomexxxx71@gmail.com>
2022-12-17 22:57:08 +0600

    done.

--- src/test/unit/xarray.spec.js
+++ src/test/unit/xarray.spec.js
@@ -6,4 +6,51 @@ describe("xarray component testing", () => {
   it("should be truthy", () => {
     expect(XArray).toBeTruthy();
   });
+  it("should expect props", () => {
+    const wrapper = mount(XArray, {
+      props: {
+        modelValue: ["one", "two"],
+      },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("should render string in separate input", () => {
+    const wrapper = mount(XArray, {
+      data() {
+        return {
+          modelValue: ["one", "two"],
+          internalValue: ["one", "two", ""],
+        };
+      },
+    });
+    const inputElements = wrapper.findAll("input");
+    expect(inputElements.length).toBe(3);
+  });
+  it("should always render extra input element", () => {
+    const wrapper = mount(XArray, {
+      data() {
+        return {
+          modelValue: ["one", "two"],
+          internalValue: ["one", "two", ""],
+        };
+      },
+    });
+    const inputs = wrapper.findAll("input");
+    expect(inputs.length).toBeGreaterThan(0);
+  });
+  it("if value is empty the element is removed from array", () => {
+    const wrapper = mount(XArray, {
+      data() {
+        return {
+          modelValue: ["one", "two"],
+          internalValue: ["one", "two", ""],
+        };
+      },
+    });
+    const input = wrapper.find("input");
+    input.element.value = "three";
+    input.trigger("input");
+    expect(wrapper.vm.internalValue.length).toBe(3);
+  });
 });
