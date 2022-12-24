af16f5fa7d910c4d40623a0b4e9f95c5051b4624
Mr. Robot <gomexxxx71@gmail.com>
2022-12-24 22:47:59 +0600

    add new code in xarray.spec.js

--- src/test/unit/xarray.spec.js
+++ src/test/unit/xarray.spec.js
@@ -2,55 +2,48 @@ import { mount } from "@vue/test-utils";
 import { describe, expect, it } from "vitest";
 import XArray from "../../components/XArray.vue";
 
-describe("xarray component testing", () => {
-  it("should be truthy", () => {
-    expect(XArray).toBeTruthy();
-  });
-  it("should expect props", () => {
+describe("testing XArray", () => {
+  it("should accept array of string in v-model", () => {
     const wrapper = mount(XArray, {
       props: {
         modelValue: ["one", "two"],
       },
     });
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.props().modelValue).toStrictEqual(["one", "two"]);
   });
 
-  it("should render string in separate input", () => {
+  // ######
+
+  it("should render each string as separate input", () => {
     const wrapper = mount(XArray, {
+      props: {
+        modelValue: ["one", "two"],
+      },
       data() {
         return {
-          modelValue: ["one", "two"],
           internalValue: ["one", "two", ""],
         };
       },
     });
-    const inputElements = wrapper.findAll("input");
-    expect(inputElements.length).toBe(3);
+
+    const input = wrapper.findAll("input").at(0);
+    expect(input.exists()).toBe(true);
   });
-  it("should always render extra input element", () => {
+
+  // #####
+  it("should be always one extra input shown, so that it is possible to add new values", () => {
     const wrapper = mount(XArray, {
+      props: {
+        modelValue: ["one", "two"],
+      },
       data() {
         return {
-          modelValue: ["one", "two"],
           internalValue: ["one", "two", ""],
         };
       },
     });
-    const inputs = wrapper.findAll("input");
-    expect(inputs.length).toBeGreaterThan(0);
-  });
-  it("if value is empty the element is removed from array", () => {
-    const wrapper = mount(XArray, {
-      data() {
-        return {
-          modelValue: ["one", "two"],
-          internalValue: ["one", "two", ""],
-        };
-      },
-    });
-    const input = wrapper.find("input");
-    input.element.value = "three";
-    input.trigger("input");
-    expect(wrapper.vm.internalValue.length).toBe(3);
+    const inputLength = wrapper.findAll("input").length;
+    const models = wrapper.props().modelValue.length;
+    expect(inputLength).toBe(models + 1);
   });
 });
